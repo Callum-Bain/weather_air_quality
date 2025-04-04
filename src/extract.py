bb27a5f6e3f9c1cf488830afffcbdd64e127b676
Hamzah Saeid <hamzahsaeid@gmail.com>
2025-04-04 16:35:53 +0100

    exception handing & logging added

--- src/extract.py
+++ src/extract.py
@@ -5,16 +5,42 @@ import os
 import pandas as pd
 from pprint import pprint
 from dotenv import load_dotenv
+import logging
 
+load_dotenv() 
 
-weather_key = os.getenv("WEATHER_KEY")
-weather_response = requests.get(f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?unitGroup=us&key={weather_key}&contentType=json')
-raw_weather_data = get_weather_data(weather_response)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler("app.log"), logging.StreamHandler()]
+)
 
-aq_key = os.getenv("AQ_KEY")
-aq_response = requests.get(f'https://api.waqi.info/feed/London/?token={aq_key}')
-raw_aq_data = get_aq_data(aq_response)
-timestamp = list(raw_aq_data.keys())[0]
+try:
+    weather_key = os.getenv("WEATHER_KEY")
+    weather_response = requests.get(f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?unitGroup=us&key={weather_key}&contentType=json')
+    raw_weather_data = get_weather_data(weather_response)
+except requests.exceptions.HTTPError as errh:
+    logging.error("HTTP Error: %s", errh)
+except requests.exceptions.ConnectionError as errc:
+    logging.error("Connection Error: %s", errc)
+except requests.exceptions.Timeout as errt:
+    logging.error("Timeout Error: %s", errt)
+except requests.exceptions.RequestException as err:
+    logging.error("Something Else: %s", err)
+
+try:
+    aq_key = os.getenv("AQ_KEY")
+    aq_response = requests.get(f'https://api.waqi.info/feed/London/?token={aq_key}')
+    raw_aq_data = get_aq_data(aq_response)
+    timestamp = list(raw_aq_data.keys())[0]
+except requests.exceptions.HTTPError as errh:
+    logging.error("HTTP Error: %s", errh)
+except requests.exceptions.ConnectionError as errc:
+    logging.error("Connection Error: %s", errc)
+except requests.exceptions.Timeout as errt:
+    logging.error("Timeout Error: %s", errt)
+except requests.exceptions.RequestException as err:
+    logging.error("Something Else: %s", err)
 
 with open(f'data/weather_output_{timestamp}.json', 'w') as file:
     json.dump(raw_weather_data, file)

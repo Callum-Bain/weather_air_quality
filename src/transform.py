96381b5a7f1d508e9532697d7b2a786a2298ea6e
Hamzah Saeid <hamzahsaeid@gmail.com>
2025-04-08 13:31:59 +0100

    transform nearly completed

--- src/transform.py
+++ src/transform.py
@@ -1,6 +1,7 @@
 from src.transform_utils import aqi_categories
 import json
 import pandas as pd
+import os
 
 def transform():
     """"" 
@@ -13,6 +14,7 @@ def transform():
         lines = timestamp_file.readlines()
         last_line = lines[-1].strip() if lines else ""
         current_date = last_line[0:10]
+        current_time = last_line[11:]
 
         with open(f'data/Extract/weather_output_{last_line}.json', 'r') as weather_file:
             weather_load = json.load(weather_file)
@@ -20,25 +22,60 @@ def transform():
 
             weather_df = pd.DataFrame(weather_data_list)
             weather_df['date'] = current_date
+            weather_df['datetime_stamp'] = last_line
             weather_df['temp'] = round((weather_df['temp'] - 32) / 1.8, 2)
             weather_df['feelslike'] = round((weather_df['feelslike'] - 32) / 1.8, 2)
-            new_weather_df = weather_df.drop(columns=['stations'], inplace=False)
-            print(new_weather_df)    
-           
+            new_weather_df = weather_df.drop(columns=['stations', 'datetimeEpoch'], inplace=False)
+            # print(new_weather_df)    
+
         with open(f'data/Extract/aq_output_{last_line}.json', 'r') as aq_file:
             aq_load = json.load(aq_file)
             aq_data_dict = aq_load[last_line]
             aqi_data = aq_load['aqi']
             aq_df = pd.DataFrame(aq_data_dict)
             aq_df['date'] = current_date
+            aq_df['datetime'] = current_time
             aq_df['AQI'] = aqi_data
             aq_df['AQI Category'] = aq_df['AQI'].apply(aqi_categories)
-            new = aq_df.reset_index(drop=True)
-     
-            # with open(f'data/Transform/weather_output_{timestamp}.json', 'w') as file:
-            #     json.dump(raw_weather_data, file)
+            new_aq_df = aq_df.reset_index(drop=True)
+            # print(new_aq_df)
 
-            # with open(f'data/Transform/aq_output_{timestamp}.json', 'w') as file:
-            #     json.dump(raw_aq_data, file)
+            # Final Weather Dataframe
+            weather_hour_df = new_weather_df[new_weather_df['datetime'].isin(aq_df['datetime'])]
+            # print(weather_hour_df)
+
+            # Final AQ Dataframe
+            new_aq_df
+
+            # final Merged Dataframe
+            merged_df = pd.merge(weather_hour_df, new_aq_df, on='datetime')
+            new_merged_df = merged_df.rename(columns={'date_x': 'date'})
+            final_merged_df = new_merged_df.drop(columns=['date_y'], inplace=False)
+            # print(final_merged_df)
+
+
+            # Create CSV weather output file
+            if not os.path.exists('data/Transform/weather_transformed_data.csv'):
+                weather_hour_df.to_csv('data/Transform/weather_transformed_data.csv', index=False)
+            elif (weather_hour_df['datetime_stamp'] != last_line).any():
+                weather_hour_df.to_csv('data/Transform/weather_transformed_data.csv', index=False, mode='a', header=False)
+            else:
+                raise Exception("No new data to append.")
+
+            # Create CSV AQ output file
+            if not os.path.exists('data/Transform/air_quality_transformed_data.csv'):
+                weather_hour_df.to_csv('data/Transform/weather_transformed_data.csv', index=False)
+            elif (weather_hour_df['datetime_stamp'] != last_line).any():
+                weather_hour_df.to_csv('data/Transform/weather_transformed_data.csv', index=False, mode='a', header=False)
+            else:
+                raise Exception("No new data to append.")     
+
+            # Create CSV Merged output file
+            if not os.path.exists('data/Transform/merged_transformed_data.csv'):
+                weather_hour_df.to_csv('data/Transform/weather_transformed_data.csv', index=False)
+            elif (weather_hour_df['datetime_stamp'] != last_line).any():
+                weather_hour_df.to_csv('data/Transform/weather_transformed_data.csv', index=False, mode='a', header=False)
+            else:
+                raise Exception("No new data to append.")       
 
 transform()

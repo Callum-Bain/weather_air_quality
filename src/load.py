69daa404da85e56a1c52ce7f37ce114e172f9012
Callum-Bain <cbain_88@hotmail.com>
2025-04-10 12:26:21 +0100

    added pseudocode

--- src/load.py
+++ src/load.py
@@ -1,2 +1,11 @@
 def load():
     pass
+
+
+
+# Load in transformed data
+# SQL schema
+# conditional logic to see if database already exists
+#  - if not then create database else continue
+# enter data into sql schema
+# load SQL schema into database
